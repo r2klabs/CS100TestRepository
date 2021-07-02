cdba80fed3361391b34cd61a3a465908c09efbb3
r2klabs <rob@r2klabs.com>
2021-07-02 15:33:59 -0400

    Added new file AddTwoNumbers.java

---
+++ AddTwoNumbers.java
@@ -0,0 +1,16 @@
+public class AddTwoNumbers{
+
+	public static void main(String args[]){
+
+		int a = 12;
+		int b = 13;
+
+		int answer = a + b;
+
+		System.out.println(answer);
+
+
+	}//end main
+
+
+}//end class
